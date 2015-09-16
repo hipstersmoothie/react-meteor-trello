4815fd3afe336f51184a2dd0652a3e6fe6ec34e5
Andrew <lisowski54@gmail.com>
2015-09-15 17:33:25 -0700

    intial

---
+++ client/app.jsx
@@ -0,0 +1,151 @@
+todoList = new Mongo.Collection("todos");
+boards = new Mongo.Collection("boards");
+var cx = React.addons.classSet;
+
+var Board = ReactMeteor.createClass({
+  templateName: "Board",
+
+  startMeteorSubscriptions: function() {
+    Meteor.subscribe("boards");
+  },
+
+  getMeteorState: function() {
+    return {
+      board: boards.findOne({ title : 'First Board' })
+    };
+  },
+
+  renderList: function(model) {
+    var todos = todoList.findOne({_id: model});
+    return <TodoList
+      key={todos._id}
+      title={todos.title}
+      todos={todos.todos}
+      board={this.state.board}
+      id={todos._id}
+      className="flex-item"
+    />;
+  },
+
+  addList: function() {
+    var newName = prompt("What is the name of the list?");
+    var boardId = this.state.board._id;
+    var newList = {
+      title: newName,
+      boardId: boardId,
+      todos: []
+    }
+    var newListId = todoList.insert(newList);
+    boards.update({_id:boardId}, {$push : { lists : newListId}})
+  },
+
+  componentDidMount: function() {
+    createSortable('.flex-container', 'x');
+  },
+
+  render: function() {
+    return <div className='col-xs-12'>
+      <h1>{this.state.board.title}</h1>
+      <div className="flex-container">
+        { this.state.board.lists.map(this.renderList) }
+        <button className="btn btn-default" type="button" onClick={this.addList}>
+          <span className="glyphicon glyphicon-plus"></span>
+        </button>
+      </div>
+    </div>;
+  }
+});
+
+var TodoList = ReactMeteor.createClass({
+  templateName: "TodoList",
+
+  startMeteorSubscriptions: function() {
+    // Meteor.subscribe("todos", this.props.title);
+  },
+
+  getMeteorState: function() {
+    return {
+      todos: todoList.findOne({_id:this.props.id}).todos
+    };
+  },
+
+  addTodo: function() {
+    var className = '.' + this.props.title.replace(/ /g,'') + '-newTodo';
+    var newTodo = { 
+      description : $(className).val() 
+    };
+
+    todoList.update(
+      { _id : this.props.id },
+      { $push : { todos : newTodo } }
+    );
+    $(className).val('');
+  },
+
+  selectTodo: function(id) {
+    Session.set("selected_todo", id);
+  },
+
+  renderTodo: function(model, i) {
+    return <Todo
+      key={i}
+      description={model.description}
+      id={this.props.id}
+      board={this.props.board}
+      onClick={this.selectTodo.bind(this, model._id)}
+    />;
+  },
+
+  deleteList: function() {
+    boards.update(
+      { _id : this.props.board._id},
+      { $pull: { lists : this.props.id } }
+    );
+  },
+
+  componentDidMount: function() {
+    createSortable('#' + this.getDOMNode().id + ' .panel-body', 'y');
+  },
+
+  render: function() {
+    var children = [];
+    var { title, todos, board, ...rest } = this.props;
+    var inputClass = title.replace(/ /g,'') + '-newTodo form-control ';
+    var wrapperClass = "flex-item " + this.props.title;
+    return <div {...rest} className={wrapperClass}>
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          {title}
+          <span className="glyphicon glyphicon-remove pull-right" onClick={this.deleteList}></span>
+        </div>
+        <div className="panel-body">
+          { this.state.todos.map(this.renderTodo) }
+         <div className="input-group">
+            <input type="text" className={inputClass} placeholder="Search for..."/>
+            <span className="input-group-btn">
+              <button className="btn btn-default" type="button" onClick={this.addTodo}>Add!</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>;
+  }
+});
+
+var Todo = React.createClass({
+  deleteTodo: function() {
+    todoList.update(
+      {_id:this.props.id},
+      { $pull: { todos : { description: this.props.description } } }
+    );
+  },
+  render: function() {
+    var { description, ...rest } = this.props;
+    return <div  {...rest} className={cx("panel panel-default", rest.className)}>
+      <div className="panel-body">
+        {description}
+        <span className="glyphicon glyphicon-remove pull-right" onClick={this.deleteTodo}></span>
+      </div>
+    </div>;
+  }
+});
---
+++ server/server.js
@@ -0,0 +1,55 @@
+todoList = new Mongo.Collection("todos");
+todoList.allow({
+  'insert': function() {
+    return true;
+  },
+  'update': function() {
+    return true;
+  },
+  'remove': function() {
+    return true;
+  },
+});
+
+// Meteor.publish("todos", function() {
+// 	return todoList.find();
+// });
+
+boards = new Mongo.Collection("boards");
+boards.allow({
+  'insert': function() {
+    return true;
+  },
+  'update': function() {
+    return true;
+  },
+  'remove': function() {
+    return true;
+  },
+});
+
+// Meteor.publish("todos", board, function() {
+// 	return board.find({title: board});
+// });
+
+function newList(boardId, name) {
+  return todoList.insert({
+    boardId: boardId,
+    title: name,
+    todos: [
+      {description: name + "todo"}
+    ]
+  });
+}
+
+Meteor.startup(function () {
+  	if(boards.find().count() < 1) {
+  		var board1Id = boards.insert({
+  			title: 'First Board',
+  			lists: []
+  		});
+      var list1 = newList(board1Id, "First list");
+      var list2 = newList(board1Id, "Second list");
+      boards.update({_id:board1Id}, {$push : { lists : { $each : [list1, list2]}}})
+  	} 
+});
